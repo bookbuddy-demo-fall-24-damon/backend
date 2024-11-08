81061f006f811b3245359903768e88170247791e
Damon Romano <rromano@creditacceptance.com>
2024-11-08 11:49:31 -0500

    First commit - boilerplate

---
+++ src/test/java/com/creditacceptance/com/bookbuddybe/BookBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditacceptance.com.bookbuddybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
